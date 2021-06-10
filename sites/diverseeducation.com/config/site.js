baabb00fa5d3f9e5c7edda5bc6137d6b5250a872
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-10 16:19:23 -0500

    Add site.js

---
+++ sites/diverseeducation.com/config/site.js
@@ -0,0 +1,53 @@
+const navigation = require('./navigation');
+const leaders = require('./leaders');
+const nativeX = require('./native-x');
+const identityX = require('./identity-x');
+const gam = require('./gam');
+
+module.exports = {
+  leaders,
+  navigation,
+  nativeX,
+  identityX,
+  gam,
+  company: 'Cox Matthews and Associates, Inc',
+  p1events: {
+    tenant: 'diverse',
+    enabled: true,
+    cookieDomain: process.env.NODE_ENV === 'production' ? 'www.diverseeducation.com' : '',
+  },
+  logos: {
+    navbar: {
+      src: 'https://img.diverseeducation.com/files/base/diverse/logo.png?h=45&auto=format,compress&q=70',
+      srcset: [
+        'https://img.diverseeducation.com/files/base/diverse/logo.png?h=90&auto=format,compress&q=70 2x',
+      ],
+    },
+    footer: {
+      src: 'https://img.diverseeducation.com/files/base/diverse/logo.png?h=60&auto=format,compress&q=70',
+      srcset: [
+        'https://img.diverseeducation.com/files/base/diverse/logo.png?h=120&auto=format,compress&q=70 2x',
+      ],
+    },
+  },
+  socialMediaLinks: [
+    { provider: 'facebook', href: 'https://www.facebook.com/DiverseIssuesInHigherEducation/' },
+    { provider: 'twitter', href: 'https://twitter.com/DiverseIssues' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/company/diverse-issues-in-higher-education/' },
+  ],
+  gtm: {
+    containerId: process.env.GTM_CONTAINER_ID || 'NOT_SET',
+  },
+  gcse: {
+    id: 'NOT_SET',
+  },
+  wufoo: {
+    userName: 'NOT_SET',
+  },
+  newsletters: {
+    subscribeLink: '',
+  },
+  magazines: {
+    description: '',
+  },
+};
