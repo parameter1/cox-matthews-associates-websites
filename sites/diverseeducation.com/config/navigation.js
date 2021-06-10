1748efda9ab115ad6bc4c89b4ffe679712315c8b
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-10 16:19:02 -0500

    Add navigation.js

---
+++ sites/diverseeducation.com/config/navigation.js
@@ -0,0 +1,33 @@
+module.exports = {
+  secondary: {
+    items: [
+    ],
+  },
+  tertiary: {
+    items: [
+      { href: '/search', label: 'Search', icon: 'search' },
+    ],
+  },
+  footer: {
+    items: [
+      { href: '/site-map', label: 'Site Map' },
+    ],
+  },
+  menu: [
+    {
+      label: 'Topics',
+      items: [
+      ],
+    },
+    {
+      label: 'Resources',
+      items: [
+      ],
+    },
+    {
+      label: 'User Tools',
+      items: [
+      ],
+    },
+  ],
+};
