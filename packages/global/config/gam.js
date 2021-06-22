87f4a7a282e186c06c68ecdea55bf5fb0f6c6525
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-22 08:54:10 -0500

    Remove unused GAM config values

--- packages/global/config/gam.js
+++ packages/global/config/gam.js
@@ -48,27 +48,6 @@ module.exports = ({
         { viewport: [750, 0], size: [[728, 90], [300, 250]] },
         { viewport: [0, 0], size: [] },
       ],
-    })
-    .setTemplate('WALLPAPER', {
-      size: [[300, 600], [100, 600]],
-      sizeMapping: [
-        { viewport: [1400, 0], size: [300, 600] },
-        { viewport: [0, 0], size: [] },
-      ],
-    })
-    .setTemplate('GEAR', {
-      size: [300, 250],
-      sizeMapping: [
-        { viewport: [980, 0], size: [300, 250] },
-        { viewport: [0, 0], size: [] },
-      ],
-    })
-    .setTemplate('GEAR-MOBILE', {
-      size: [300, 250],
-      sizeMapping: [
-        { viewport: [980, 0], size: [] },
-        { viewport: [300, 0], size: [300, 250] },
-      ],
     });
 
   return config;
