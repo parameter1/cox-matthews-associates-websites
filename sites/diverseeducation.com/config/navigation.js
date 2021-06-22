e4ba80188025d3dbd52f2635b45bb7b287ecf0eb
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-22 12:03:16 -0500

    Update nav & menu items

--- sites/diverseeducation.com/config/navigation.js
+++ sites/diverseeducation.com/config/navigation.js
@@ -1,17 +1,37 @@
-const topics = {
-  primary: [
-    { href: '/latest-news', label: 'News' },
-    { href: '/opinion', label: 'Opinion' },
-    { href: '/campus-climate', label: 'Campus Climate' },
-    { href: '/international', label: 'International' },
-    { href: '/disabilities', label: 'Disabilities' },
-    { href: '/sports', label: 'Sports' },
-  ],
-  expanded: [
-    { href: '/covid-19', label: 'COVID-19' },
-  ],
-  secondary: [],
-};
+const topics = [
+  { href: '/african-american', label: 'African-American' },
+  { href: '/asian-american-pacific-islander', label: 'Asian American Pacific Islander' },
+  { href: '/campus-climate', label: 'Campus Climate' },
+  { href: '/covid-19', label: 'COVID-19' },
+  { href: '/disabilties', label: 'Disabilties' },
+  { href: '/faculty-issues', label: 'Faculty Issues' },
+  { href: '/hbcus', label: 'HBCUs' },
+  { href: '/international', label: 'International ' },
+  { href: '/latinx', label: 'Latinx' },
+  { href: '/leadership', label: 'Leadership' },
+  { href: '/lgbtq+', label: 'LGBTQ+' },
+  { href: '/msis', label: 'MSIs' },
+  { href: '/native-americans', label: 'Native Americans' },
+  { href: '/recruitment-retention', label: 'Recruitment & Retention' },
+  { href: '/social-justice', label: 'Social Justice' },
+  { href: '/sports', label: 'Sports' },
+  { href: '/stem', label: 'STEM' },
+  { href: '/student-issues', label: 'Student Issues' },
+  { href: '/tenure', label: 'Tenure' },
+  { href: '/women', label: 'Women' },
+];
+
+const resources = [
+  { href: '/community-colleges', label: 'Community Colleges' },
+  { href: '/military', label: 'Military' },
+  { href: '/health', label: 'Health' },
+  { href: '/sponsored-content', label: 'Sponsored Content' },
+  { href: '/latest-news', label: 'Latest News' },
+  { href: '/opinion', label: 'Opinion' },
+  { href: '/reports-data', label: 'Reports & Data' },
+  { href: '/from-the-magazine', label: 'From the Magazine' },
+  { href: '/jobs', label: 'Jobs' },
+];
 
 const utilities = [
   { href: '/page/advertise', label: 'Advertise' },
@@ -21,11 +41,9 @@ const utilities = [
 
 const mobileMenu = {
   primary: [
-    ...topics.primary,
-    ...topics.expanded,
+    ...topics,
   ],
   secondary: [
-    ...topics.secondary,
     { href: '/newsletters', label: 'Newsletters' },
   ],
 };
@@ -33,9 +51,7 @@ const mobileMenu = {
 const desktopMenu = {
   about: [...utilities],
   sections: [
-    ...topics.primary,
-    ...topics.expanded,
-    ...topics.secondary,
+    ...topics,
   ],
 };
 
@@ -47,7 +63,14 @@ module.exports = {
     items: [],
   },
   secondary: {
-    items: topics.primary,
+    items: [
+      { href: '/latest-news', label: 'News' },
+      { href: '/opinion', label: 'Opinion' },
+      { href: '/campus-climate', label: 'Campus Climate' },
+      { href: '/international', label: 'International' },
+      { href: '/disabilities', label: 'Disabilities' },
+      { href: '/sports', label: 'Sports' },
+    ],
   },
   tertiary: {
     items: [],
@@ -55,14 +78,12 @@ module.exports = {
   toggleMenu: {
     midColumn: {
       items: [
-        ...topics.primary,
-        ...topics.expanded,
+        ...topics,
       ],
     },
     rightColumn: {
       items: [
-        ...topics.secondary,
-        ...utilities,
+        ...resources,
       ],
     },
   },
