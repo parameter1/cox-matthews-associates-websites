0cf0865093cc315d107e1f5d9896a2bfea730aa6
Brandon Krigbaum <brandonbk@gmail.com>
2021-07-26 18:24:06 -0500

    Add navigation configs

---
+++ sites/ccnewsnow.com/config/navigation.js
@@ -0,0 +1,64 @@
+const topics = [
+];
+
+const resources = [
+];
+
+const utilities = [
+  { href: '/page/advertise', label: 'Advertise' },
+  { href: '/page/contact-us', label: 'Contact Us' },
+  { href: '/newsletters', label: 'Newsletters' },
+];
+
+const mobileMenu = {
+  primary: [
+    ...topics,
+  ],
+  secondary: [
+    { href: '/newsletters', label: 'Newsletters' },
+  ],
+};
+
+const desktopMenu = {
+  about: [...utilities],
+  sections: [
+    ...topics,
+  ],
+};
+
+module.exports = {
+  desktopMenu,
+  mobileMenu,
+  topics,
+  primary: {
+    items: [],
+  },
+  secondary: {
+    items: [
+    ],
+  },
+  tertiary: {
+    items: [],
+  },
+  toggleMenu: {
+    midColumn: {
+      items: [
+        ...topics,
+      ],
+    },
+    rightColumn: {
+      items: [
+        ...resources,
+      ],
+    },
+  },
+  footer: {
+    items: [
+      { href: '/privacy-policy', label: 'Privacy Policy' },
+      { href: '/page/contact-us', label: 'Contact Us' },
+      { href: '/site-map', label: 'Site Map' },
+    ],
+    topics: topics.primary,
+    more: utilities,
+  },
+};
