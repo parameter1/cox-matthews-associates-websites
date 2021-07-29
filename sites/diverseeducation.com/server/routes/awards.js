51a4743f4195f136b60e4a1612c271ab03e14457
Brian Miller <brian@parameter1.com>
2021-07-29 15:36:55 -0500

    add champions route

--- sites/diverseeducation.com/server/routes/awards.js
+++ sites/diverseeducation.com/server/routes/awards.js
@@ -1,7 +1,14 @@
+const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
+const queryFragment = require('@cox-matthews-associates/package-global/graphql/fragments/website-section-page');
 const emergingScholars = require('./awards/emerging-scholars');
-
+const champions = require('../templates/website-section/awards-honors/champions');
 
 module.exports = (app) => {
+  app.get('/:alias(awards-honors/diverse-champions)', withWebsiteSection({
+    template: champions,
+    queryFragment,
+  }));
+
   // Emerging Scholars
   emergingScholars(app);
 };
