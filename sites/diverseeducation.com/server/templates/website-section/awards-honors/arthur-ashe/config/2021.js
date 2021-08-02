ebf26bbc8da3b615c485c4b6a32c1a89f5a5fb28
Brian Miller <brian@parameter1.com>
2021-08-02 12:55:56 -0500

    setup/move arthur ashe to website section layout

---
+++ sites/diverseeducation.com/server/templates/website-section/awards-honors/arthur-ashe/config/2021.js
@@ -0,0 +1,122 @@
+module.exports = {
+  winners: {
+    men: {
+      profiles: [
+        {
+          name: 'D\'Eriq King',
+          thumbnail: 'DERIQ-KING.png',
+          body: 'Over the course of his college football career, University of Miami quarterback <strong>D’Eriq King</strong> has overcome injury, transferring and the sudden death of his father. Despite adversity, his sense of purpose has not wavered. King is intent on being a leader on the field, excelling in the classroom and speaking up about social justice issues.',
+        },
+      ],
+    },
+    women: {
+      profiles: [
+        {
+          name: 'Mariah White',
+          thumbnail: 'MARIAH-WHITE.png',
+          body: '<strong>Mariah White</strong> knows how to make the most out of opportunities. The senior point guard at Cleveland State University was determined to play Division I basketball, and CSU came calling. It proved to be the perfect fit for the Ohio native, who has excelled on the court, in the classroom and as the Student-Athlete Advisory Committee’s (SAAC) first diversity, equity and inclusion (DEI) chair. For her impact on her sport, teammates, friends and community, White is named the 2021 Arthur Ashe Jr. Female Sports Scholar.',
+        },
+      ],
+    },
+  },
+  finalists: {
+    men: {
+      profiles: [
+        {
+          name: 'Samuel Cudjoe',
+          thumbnail: 'SAMUEL-CUDJOE.png',
+          body: 'Senior sports management major <b>Samuel Cudjoe</b> found immediate success his first year as a freshman golfer at Saint Augustine’s University. A native of Bethel, Trinidad &amp; Tobago, Cudjoe won both the Central Intercollegiate Athletic Association (CIAA) Golfer of the Year and CIAA Rookie Golfer of the Year awards in 2018. He then qualified for the NCAA Division II Atlantic/East Regional Tournament As The Top Individual Seed That Same Year. A Two-Time All-Ciaa Selection, Cudjoe Holds The School Record For The Lowest Individual One-Day Score (65) Through 18 Holes. He Is A Two-Time Arthur Ashe Jr. Sports Scholar And Is Also A Cosida Division Ii Academic All-District 2 Golfer.',
+        },
+        {
+          name: 'Daniel Do',
+          thumbnail: 'DANIEL-DO.png',
+          body: 'Track and cross-country runner <b>Daniel Do</b> is a senior biochemistry and molecular biology major at Stockton University and has maintained a 4.0 GPA, all while making impressive strides as a runner. During the 2020 track and field season, he set a new school record with a personal best of 8:41.99 in the 3000 meters and finished seventh out of 25 at the Fastrack Season Opener. During the 2019 cross country season, he was Stockton’s top finisher in all seven races and finished fourth at the New Jersey Athletic Conference Championships. He is a CoSIDA Academic All-American athlete and a member of the American Society for Biochemistry and Molecular Biology Honor Society.',
+        },
+        {
+          name: 'Kamaka Hepa',
+          thumbnail: 'KAMAKA-HEPA.png',
+          body: 'University of Texas at Austin’s <b>Kamaka Hepa</b> finished his first two basketball seasons strong having played in 51 games, including 11 games as a starter. Returning for his third season in the frontcourt rotation, Hepa has built impressive stats for himself, having personal bests of 15 points scored in one game, five free throws made in one game, three rebounds made in one game and 26 minutes played in one game. A senior sports management major, Hepa has made the Big 12 Commissioner’s Honor Roll two years in a row. Originally from Alaska and part Inupiat (Eskimo), Hepa hopes to become the first Inupiat to play in the NBA.',
+        },
+        {
+          name: 'Paolo Marciano',
+          thumbnail: 'PAOLO-MARCIANO.png',
+          body: 'Senior management major <b>Paolo Marciano</b> “has already left an indelible mark” on the soccer program at Adelphi University. His career accomplishments thus far include being recognized as the Northeast-10 (NE10) Player of the Year and Eastern College Athletic Conference (ECAC) Offensive Player of the Year, as well as the NE10 and ECAC Rookie of the Year. Marciano is also a two-time NE10 All-Conference and Division 2 Conference Commissioners Association (D2CCA) All-Region performer, and he was honored as the program\'s sixth United Soccer Coaches First Team All-American in 2019. With a cumulative GPA of 3.81, he was named To The Ne10 Academic Honor Roll Four Times And Is A Division 2 Athletics Directors Association Academic Achievement Award Recipient.',
+        },
+        {
+          name: 'Adrian Martinez',
+          thumbnail: 'ADRIAN-MARTINEZ.png',
+          body: 'The University of Nebraska, Lincoln’s <b>Adrian Martinez</b>, a senior management major, made football history in 2020 when he completed 71.5% of his passes, a Husker season record, ranking him fourth in Big Ten history. In addition to ranking fourth nationally in completion percentage, Martinez was second nationally in rushing yards per game among all FBS quarterbacks (74.4) and was the only Power Five quarterback to average 70 rushing yards per game. He was the Nebraska Offensive Back of the Year in 2018 and 2019, a freshman All-American in 2018 and was Nebraska’s Outstanding Male Newcomer in the 2018-19 season.',
+        },
+        {
+          name: 'Andre Mintze',
+          thumbnail: 'ANDRE-MINTZE.png',
+          body: '<b>Andre Mintze</b>, a graduate student in medicine, health, and society at Vanderbilt University, is a two-time football captain who has taken on a variety of leadership roles for the team. A starting linebacker for the past two years, he’s helped Vanderbilt to an appearance in the 2018 Texas Bowl as well as consecutive victories against in-state rival Tennessee. He’s played in 43 games for the Commodores, making 19 starts, and was a 2020 preseason All-Southeastern Conference selection, totaling 75 tackles, 9.5 sacks, five quarterback pressures and two forced fumbles. He has been on the SEC Honor Roll for four years.',
+        },
+        {
+          name: 'Shea-Pitts',
+          thumbnail: 'SHEA-PITTS.png',
+          body: 'University of California, Los Angeles reserve linebacker <b>Shea Pitts</b> saw action in five games during the shortened 2020 football season. In the prior two seasons, he played in 23 out of 24 games, tallying 13 tackles, his first career forced fumble and his first career sack. A senior political science major, Pitts has made the UCLA Athletic Director\'s Honor Roll on nine occasions. Off campus, he is a mentor to children from the Los Angeles Unified School District and has raised money for Court Appointed Special Advocates, the Los Angeles Blind Children’s Center and victims of genocide in Myanmar through different on-campus endeavors.',
+        },
+        {
+          name: 'Jeronimo Valdepena',
+          thumbnail: 'JERONIMO-VALDEPENA.png',
+          body: 'California State University, San Bernardino\'s (CSUSB) <b>Jeronimo Valdepena</b> is a senior kinesiology major and soccer player with a heart for community service. A two-year starter, Valdepena started 11 out of the 21 games he played in the 2019 season, in which he scored one goal and had four assists for the Yotes. In the 2018 season, he started 12 out of 19 games played and had a total of two goals and four assists. Off the field, he’s served in an array of volunteer positions, mentoring local high school students, raising voting awareness and creating care packages for the homeless, among other jobs. In the classroom he is a two-time California Collegiate Athletic Association All-Academic honoree and has been on the Dean\'s List his entire tenure at CSUSB.',
+        },
+        {
+          name: 'Tyler Williams',
+          thumbnail: 'TYLER-WILLIAMS.png',
+          body: 'California State University, San Bernardino’s <b>Tyler Williams</b> is a force to be reckoned with on the baseball diamond. The lead pitcher in 2019, he started in all 12 games played, struck out 67 batters and pitched a total of 75.2 innings. In the shortened 2020 baseball season, he made six appearances with a 4.91 earned run average. A senior communications major, Williams has maintained a 3.8 GPA, making him an ongoing member of the Dean’s list since his first semester and earning recognition from the California Collegiate Athletic Association. Amid the pandemic, he’s donated time as a volunteer at a local COVID-19 vaccination site.',
+        },
+      ],
+    },
+    women: {
+      profiles: [
+        {
+          name: 'Riley Agerbeek',
+          thumbnail: 'RILEY-AGERBEEK.png',
+          body: 'Water polo player <b>Riley Agerbeek</b>, a senior justice studies major at San José State University, achieved her best season statistically in 2020, scoring 18 goals in 12 matches for the San José State water polo team. Agerbeek is a two-time Mountain Pacific Sports Federation All-Academic team honoree and was a member of the 2018, 2019 and 2020 Association of Collegiate Water Polo Coaches all-academic teams. She is currently serving as team representative on the San José State University Student-Athlete Advisory Committee (SAAC) for the second year in a row and is the SAAC publicity chair for the 2020-21 academic year.',
+        },
+        {
+          name: 'Jessica Drop',
+          thumbnail: 'JESSICA-DROP.png',
+          body: '<b>Jessica Drop</b>, a senior cellular biology major, has set the pace at the University of Georgia, creating several new school records over the course of her track &amp; field career. She ran the indoor 3000 meters at 9:11, the indoor 5000 meters at 15:43 and the outdoor 5000 meters at 15:38. She is a 2018, 2019 and 2020 All-American athlete and was named The Southeastern Conference’s 2020 Women’s Co-Scholar Athlete Of The Year. A Devoted Volunteer And Community Service Leader, Drop Has Maintained A Cumulative Gpa Of 3.9.',
+        },
+        {
+          name: 'Lena Greene',
+          thumbnail: 'LENA-GREENE.png',
+          body: 'Senior bioengineering major <b>Lena Greene</b> is an All-American gymnast for Oregon State University (OSU), where she has successfully ‘hit’ 15 out of her 16 career routines and has secured two event titles on vault with a high score of 9.9. Greene is an active member of OSU’s Dam Worth It campaign, a student-athlete mental health awareness campaign, and is a member of the PAC-12’s newly-formed Gymnasts for Peace, Action, and Change (G-PAC), created to further conversations around diversity, equity and inclusion. A dedicated volunteer for various nonprofits, Greene has logged more than 94 hours of community service work throughout her collegiate career.',
+        },
+        {
+          name: 'Touri Holmes',
+          thumbnail: 'TOURI-HOLMES.png',
+          body: 'Morgan State University’s <b>Touri Holmes</b> is a senior computer science major and an impressive bowler. She has bowled four consecutive 300-point games — the highest possible score — achieved by bowling 12 strikes in a row. Among other awards, she is a regular dean’s list member, MEAC Scholar-Athlete and All-American Scholar with a cumulative GPA of 3.89. Despite the pandemic, she recently served as a virtual volunteer for a Baltimore organization called Code in the Schools, introducing computer science to children as early as elementary school.',
+        },
+        {
+          name: 'Britt Larson-Jackson',
+          thumbnail: 'BRITT-LARSON-JACKSON.png',
+          body: 'Equestrian <b>Britt Larson-Jackson</b> soars over fences at Sweet Briar College and makes history while she’s at it — she currently holds the 10th highest score over fences at the school. As part of the National Collegiate Equestrian Association (NCEA), she has competed in eight meets over fences and five on the flat for Sweet Briar, with a career record of 1-7-0 and 2-2-1 over fences and on the flat, respectively. When not in the saddle, Larson-Jackson, a junior business major, volunteers her time for various organizations, working with the children’s ministry at her local church and serving as the diversity representative for Sweet Briar\'s Student Athlete Advisory Committee. She is a two-time Old Dominion Athletic Conference All-Academic Team member, she is also a two-time NCEA Academic Honor Roll member and a five-time member of the Dean’s List.',
+        },
+        {
+          name: 'Alisha Lau',
+          thumbnail: 'ALISHA-LAU.png',
+          body: 'Golfer <b>Alisha Lau</b>, a senior media production major at the University of Colorado, Boulder, didn’t take a break when the Pac-12 canceled its fall season. Instead, she teamed up with teammate Hailey Schalk for the United States Golf Association’s 4-ball qualifier. Together, the duo won, carding a 4-under 68. Lau was also named To The 2021 Golf Canada Women’s Amateur Team In November. In The 2019-20 Season, Lau Led Cu To Five Top-20 Finishes And Wrapped Up The Fall By Shooting Under 220 At The Final Two Tournaments. She Is A 2019-20 Pac-12 Academic Honor Roll Member And A 2019-20 Women’s Golf Coaches Association All-American Scholar.',
+        },
+        {
+          name: 'Savanna Ly-Nguyen',
+          thumbnail: 'SAVANNA-LY-NGUYEN.png',
+          body: 'Washington State University (WSU) tennis player <b>Savanna Ly-Nguyen</b> finished the 2020 season strong with a 16-10 singles record and is carrying that same success into the 2021 season, currently holding a team-best season record of 17-2 as of April 11. A junior neuroscience major, Ly-Nguyen has found success academically too. As a two-time recipient of the Intercollegiate Tennis Association Academic Achievement Award, she currently holds a cumulative GPA of 3.95 and is a regular volunteer at WSU Athletics community service events. In 2019, she represented Vietnam at the Southeast Asian Games, where she captured a silver medal.',
+        },
+        {
+          name: 'Macy Somoskey',
+          thumbnail: 'MACY-SOMOSKEY.png',
+          body: 'University of Mississippi golfer <b>Macy Somoskey</b> is a talented senior mathematics major who has maintained a perfect 4.0 GPA throughout her collegiate career, earning her a regular spot on the dean’s list and honor roll. A previous Women’s Golf Coaches Association All-American Scholar, she’s proven herself talented on the greens too. Before spring golf was canceled mid-season due to COVID-19, Somoskey posted a 75.5 stroke average. In the 2018-19 season, she averaged 77.05 strokes and turned in match-deciding victories for Ole Miss at the SEC Championship, playing 63 holes of match play in total. It was Ole Miss’ first-ever SEC title.',
+        },
+        {
+          name: 'Chanel Thomas',
+          thumbnail: 'CHANEL-THOMAS.png',
+          body: '<b>Chanel Thomas</b> is a master of business administration student at the University of Mississippi who’s played in a total of 69 games over the course of her soccer career, totaling 4,464 minutes on the field. In addition to her athletic success, Thomas has maintained a 4.0 GPA and was awarded the Marcus Elvis Taylor Memorial Medal Designee — an academic honor given to no more than 1% of the student body. As Ole Miss soccer team academic captain for three years, she helped her teammates progress academically and assisted coaches in upholding academic standards for the program. She was on the University of Mississippi Chancellor’s Honor Roll for six consecutive semesters.',
+        },
+      ],
+    },
+  },
+};
