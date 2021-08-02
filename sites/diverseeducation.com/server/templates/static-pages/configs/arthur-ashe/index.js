575aa006e168cb4442a1f0bf251a819bbafff70c
Brian Miller <brian@parameter1.com>
2021-08-02 12:57:45 -0500

    remove static page version of arthur ashe pages

--- sites/diverseeducation.com/server/templates/static-pages/configs/arthur-ashe/index.js
+++
@@ -1,8 +0,0 @@
-const year2021 = require('./2021');
-const year2020 = require('./2020');
-
-module.exports = {
-  'awards-honors/arthur-ashe': year2021,
-  'awards-honors/arthur-ashe/2021': year2021,
-  'awards-honors/arthur-ashe/2020': year2020,
-};
