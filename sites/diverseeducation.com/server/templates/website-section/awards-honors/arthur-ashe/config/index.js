9893f6c84d672d57374c5590c6c00fad82961a66
Brian Miller <brian@parameter1.com>
2021-08-03 09:53:26 -0500

    add a main config to display only the 2021 winners
    This will show the 2021 winners on the main Arthur Ashe awards landing page.

--- sites/diverseeducation.com/server/templates/website-section/awards-honors/arthur-ashe/config/index.js
+++ sites/diverseeducation.com/server/templates/website-section/awards-honors/arthur-ashe/config/index.js
@@ -1,7 +1,9 @@
+const main = require('./main');
 const year2021 = require('./2021');
 const year2020 = require('./2020');
 
 module.exports = {
+  'awards-honors/arthur-ashe': main,
   'awards-honors/arthur-ashe/2021': year2021,
   'awards-honors/arthur-ashe/2020': year2020,
 };
