761aefec301918c844a3fd4a40a489d3d829c746
Brian Miller <brian@parameter1.com>
2021-08-04 13:12:22 -0500

    Update logic to match flattened section structure

--- sites/diverseeducation.com/server/graphql/fragments/section-with-children.js
+++ sites/diverseeducation.com/server/graphql/fragments/section-with-children.js
@@ -11,24 +11,6 @@ fragment WebsiteSectionWithChilderenFragment on WebsiteSection {
         id
         alias
         name
-        children(input: { pagination: { limit: 0 }, sort: { field: sequence, order: asc } }) {
-          edges {
-            node {
-              id
-              alias
-              name
-              children(input: { pagination: { limit: 0 }, sort: { field: sequence, order: asc } }) {
-                edges {
-                  node {
-                    id
-                    alias
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
       }
     }
   }
