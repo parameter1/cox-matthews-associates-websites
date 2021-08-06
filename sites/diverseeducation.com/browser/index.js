a83cd644ce4da6df698447c72e789badfdc8e5aa
Brian Miller <brian@parameter1.com>
2021-08-06 12:29:57 -0500

    Update component name to SiteSalaryBarChart

--- sites/diverseeducation.com/browser/index.js
+++ sites/diverseeducation.com/browser/index.js
@@ -3,11 +3,11 @@ import DefaultTheme from '@parameter1/base-cms-marko-web-theme-default/browser';
 import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import Global from '@cox-matthews-associates/package-global/browser';
 
-const SalaryBarChart = () => import(/* webpackChunkName: "salary-bar-chart" */ './salary-bar-chart.vue');
+const SalaryBarChart = () => import(/* webpackChunkName: "site-salary-bar-chart" */ './salary-bar-chart.vue');
 
 Global(Browser);
 DefaultTheme(Browser);
 GCSE(Browser);
-Browser.register('SalaryBarChart', SalaryBarChart);
+Browser.register('SiteSalaryBarChart', SalaryBarChart);
 
 export default Browser;
