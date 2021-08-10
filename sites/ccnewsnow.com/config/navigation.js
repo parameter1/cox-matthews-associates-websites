30216c3608d44f6ace96215ad8dc4b7ab54692c5
Brandon Krigbaum <brandonbk@gmail.com>
2021-08-09 19:39:17 -0500

    Update newsletter signup links within navigation

--- sites/ccnewsnow.com/config/navigation.js
+++ sites/ccnewsnow.com/config/navigation.js
@@ -15,7 +15,7 @@ const resources = [
 const utilities = [
   { href: '/page/advertise', label: 'Advertise' },
   { href: '/contact-us', label: 'Contact Us' },
-  { href: '/newsletters', label: 'Newsletters' },
+  { href: 'https://responses.diverseeducation.com/CCNewsNow', label: 'Newsletters', target: '_blank' },
 ];
 
 const mobileMenu = {
