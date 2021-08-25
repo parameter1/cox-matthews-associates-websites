c0f28b2429debb2d3e73f79828bbd31abaa41b1b
Brandon Krigbaum <brandonbk@gmail.com>
2021-08-24 21:13:54 -0500

    Add podcasts template and css

--- sites/diverseeducation.com/server/routes/website-section.js
+++ sites/diverseeducation.com/server/routes/website-section.js
@@ -1,12 +1,17 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@cox-matthews-associates/package-global/graphql/fragments/website-section-page');
 const cards = require('../templates/website-section/cards');
+const podcasts = require('../templates/website-section/podcasts');
 const products = require('../templates/website-section/products');
 const section = require('../templates/website-section');
 
 module.exports = (app) => {
   app.get('/:alias(opinion)', withWebsiteSection({
     template: cards,
+    queryFragment,
+  }));
+  app.get('/:alias(podcasts)', withWebsiteSection({
+    template: podcasts,
     queryFragment,
   }));
   app.get('/:alias(products)', withWebsiteSection({
