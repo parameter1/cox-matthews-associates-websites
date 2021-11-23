7db86ef08534e96f792b7d0bb4f6814bb9484956
Brian Miller <brian@parameter1.com>
2021-11-23 06:44:47 -0600

    udpate fragment, route and styles

--- sites/diverseeducation.com/server/routes/website-section.js
+++ sites/diverseeducation.com/server/routes/website-section.js
@@ -1,5 +1,6 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@cox-matthews-associates/package-global/graphql/fragments/website-section-page');
+const queryFragmentWithLogo = require('@cox-matthews-associates/package-global/graphql/fragments/website-section-with-logo-page');
 const cards = require('../templates/website-section/cards');
 const podcasts = require('../templates/website-section/podcasts');
 const products = require('../templates/website-section/products');
@@ -14,7 +15,7 @@ module.exports = (app) => {
   }));
   app.get('/:alias(podcasts)', withWebsiteSection({
     template: podcasts,
-    queryFragment,
+    queryFragment: queryFragmentWithLogo,
   }));
   app.get('/:alias(products)', withWebsiteSection({
     template: products,
