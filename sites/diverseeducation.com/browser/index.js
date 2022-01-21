497caec5366015fa21ca1d0bd26a8965c0357c6b
Brian Miller <brian@parameter1.com>
2022-01-21 09:13:35 -0600

    Clean up brwoser inclusions in sites
    Remove the default inclusion as its not need & move GSCE to global.

--- sites/diverseeducation.com/browser/index.js
+++ sites/diverseeducation.com/browser/index.js
@@ -1,13 +1,9 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
-import DefaultTheme from '@parameter1/base-cms-marko-web-theme-default/browser';
-import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import Global from '@cox-matthews-associates/package-global/browser';
 
 const SalaryBarChart = () => import(/* webpackChunkName: "site-salary-bar-chart" */ './salary-bar-chart.vue');
 
 Global(Browser);
-DefaultTheme(Browser);
-GCSE(Browser);
 Browser.register('SiteSalaryBarChart', SalaryBarChart);
 
 export default Browser;
