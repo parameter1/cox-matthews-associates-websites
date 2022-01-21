e063d8bd0f4ef5900fd16d5c942e8a369667bed0
Brian Miller <brian@parameter1.com>
2022-01-21 07:33:43 -0600

    remove un used global vue components

--- packages/global/browser/ssr.js
+++ packages/global/browser/ssr.js
@@ -1,9 +1,5 @@
 import GlobalMenuToggleButton from './menu-toggle-button.vue';
-import GlobalNewsletterToggleButton from './newsletter-toggle-button.vue';
-import GlobalSiteNewsletterMenu from './site-newsletter-menu.vue';
 
 export default {
   GlobalMenuToggleButton,
-  GlobalNewsletterToggleButton,
-  GlobalSiteNewsletterMenu,
 };
