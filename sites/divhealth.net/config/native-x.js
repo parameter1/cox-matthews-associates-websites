ca405b6a1867f128f5aa14563ec9897ad80fe7ed
Brandon Krigbaum <brandonbk@gmail.com>
2022-01-31 13:54:44 -0600

    Fix native-x uri

--- sites/divhealth.net/config/native-x.js
+++ sites/divhealth.net/config/native-x.js
@@ -1,6 +1,6 @@
 const NativeXConfiguration = require('@parameter1/base-cms-marko-web-native-x/config');
 
-const config = new NativeXConfiguration('https://diverseeducation.native-x.parameter1.com');
+const config = new NativeXConfiguration('https://diverse.native-x.parameter1.com');
 
 config.enabled = true;
 
