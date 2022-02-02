10bbc0fe21a651b2178409e2d01e625ea4ab6f81
Brandon Krigbaum <brandonbk@gmail.com>
2022-02-02 11:19:15 -0600

    Enable and configure identity-x

--- packages/global/config/identity-x.js
+++ packages/global/config/identity-x.js
@@ -1,9 +1,5 @@
 const IdentityXConfiguration = require('@parameter1/base-cms-marko-web-identity-x/config');
-const rapidOmedaIdentityX = require('@parameter1/base-cms-marko-web-omeda-identity-x/rapid-identify');
-const { get } = require('@parameter1/base-cms-object-path');
 const newrelic = require('newrelic');
-
-const omedaConfig = require('./omeda');
 
 module.exports = ({
   appId,
@@ -15,17 +11,6 @@ module.exports = ({
     apiToken,
     requiredServerFields,
     onHookError: newrelic.noticeError.bind(newrelic),
-  });
-  config.addHook({
-    name: 'onUserProfileUpdate',
-    shouldAwait: false,
-    fn: async ({ user, service, req }) => rapidOmedaIdentityX({
-      brandKey: omedaConfig.brandKey,
-      productId: get(omedaConfig, 'rapidIdentification.productId'),
-      appUser: user,
-      identityX: service,
-      omedaGraphQL: req.$omeda,
-    }),
   });
   return config;
 };
--- sites/ccnewsnow.com/config/identity-x.js
+++ sites/ccnewsnow.com/config/identity-x.js
@@ -1,3 +1,3 @@
 const configureIdentityX = require('@cox-matthews-associates/package-global/config/identity-x');
 
-module.exports = configureIdentityX({ appId: 'TBD' });
+module.exports = configureIdentityX({ appId: '60b7b2f61245f4dd8ae69e1f' });
