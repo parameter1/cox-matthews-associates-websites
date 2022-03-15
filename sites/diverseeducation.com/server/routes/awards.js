ed8dcd08f8241f9f1fb114ab263faf128ef28885
Jessica Wade <jessica.wade@parameter1.com>
2022-03-15 14:56:21 -0500

    Update routes to use ‘mppwsaYearly’ template

--- sites/diverseeducation.com/server/routes/awards.js
+++ sites/diverseeducation.com/server/routes/awards.js
@@ -6,9 +6,6 @@ const emergingScholars = require('./awards/emerging-scholars');
 const graduateScholars = require('./awards/graduate-scholars');
 const champions = require('../templates/website-section/awards-honors/champions');
 const arthurAshe = require('../templates/website-section/awards-honors/arthur-ashe');
-const mppwsa = require('../templates/static-pages/mppwsa');
-const mppwsa2021 = require('../templates/static-pages/mppwsa-2021');
-const mppwsa2020 = require('../templates/static-pages/mppwsa-2020');
 const mppwsaYearly = require('../templates/static-pages/mppwsa-yearly');
 const mppwcc = require('../templates/static-pages/mppwcc');
 
@@ -28,22 +25,12 @@ module.exports = (app) => {
 
   app.get('/:alias(awards-honors/mppwsa)', asyncRoute(async (req, res) => {
     const { alias } = req.params;
-    return res.marko(mppwsa, { alias });
+    return res.marko(mppwsaYearly, { alias });
   }));
 
   app.get('/:alias(awards-honors/mppwsa/\\d{4})', asyncRoute(async (req, res) => {
     const { alias } = req.params;
     return res.marko(mppwsaYearly, { alias });
-  }));
-
-  app.get('/:alias(awards-honors/mppwsa/2021)', asyncRoute(async (req, res) => {
-    const { alias } = req.params;
-    return res.marko(mppwsa2021, { alias });
-  }));
-
-  app.get('/:alias(awards-honors/mppwsa/2020)', asyncRoute(async (req, res) => {
-    const { alias } = req.params;
-    return res.marko(mppwsa2020, { alias });
   }));
 
   app.get('/:alias(awards-honors/arthur-ashe/\\d{4})', withWebsiteSection({
