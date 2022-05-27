887e1009d70a4f73603710239ea6e518960c6459
Brian Miller <brian@parameter1.com>
2022-05-27 09:30:19 -0500

    Consolidate recaptcha usage to V3 with P1 Dep upgrades
    @parameter1/base-cms upgrades along with consolidating the usage of recaptcha to V3 invisible version.

--- sites/diverseeducation.com/config/contact-us.js
+++ sites/diverseeducation.com/config/contact-us.js
@@ -1,4 +1,7 @@
 module.exports = {
+  branding: {
+    logo: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/diverse-education-logo.png?h=60&auto=format,compress&q=70',
+  },
   submitPodcast: {
     title: 'Submit A podcast',
     to: 'maya@diverseeducation.com',
