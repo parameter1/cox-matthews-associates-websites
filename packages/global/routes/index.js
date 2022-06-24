c24b56292792d7e9b295834f7af544d95bbc5eaa
Brandon Krigbaum <brandonbk@gmail.com>
2022-06-23 22:01:41 -0500

    Replace local magazine components with monorail version

--- packages/global/routes/index.js
+++ packages/global/routes/index.js
@@ -1,10 +1,10 @@
 const htmlSitemap = require('@parameter1/base-cms-marko-web-html-sitemap/routes');
 const renderBlock = require('@parameter1/base-cms-marko-web-theme-monorail/routes/render-block');
 const taxonomy = require('@parameter1/base-cms-marko-web-theme-monorail/routes/taxonomy');
+const magazine = require('@parameter1/package-theme-monorail-magazine/routes');
 const dynamicPage = require('./dynamic-page');
 const feed = require('./feed');
 // const identityX = require('./identity-x');
-const magazine = require('../components/magazine/routes');
 const nativeX = require('./native-x');
 const printContent = require('./print-content');
 const publicFiles = require('./public-files');
