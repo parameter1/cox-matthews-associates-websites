e20d3c906568c7ee709b0c3c3c38806248aeaf2b
Jessica Wade <jessica.wade@parameter1.com>
2022-07-28 14:27:04 -0500

    Create top-women route under awards

---
+++ sites/diverseeducation.com/server/routes/awards/top-women.js
@@ -0,0 +1,17 @@
+const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
+const { asyncRoute, cleanPath } = require('@parameter1/base-cms-utils');
+const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/website-section-page');
+const topWomen = require('../../templates/website-section/awards-honors/top-women');
+
+
+module.exports = (app) => {
+  app.get('/:alias(awards-honors/top-women)', asyncRoute(async (req, res) => {
+    const { alias } = req.params;
+    res.redirect(301, `/${cleanPath(alias)}/2022`);
+  }));
+
+  app.get('/:alias(awards-honors/top-women/\\d{4})', withWebsiteSection({
+    template: topWomen,
+    queryFragment,
+  }));
+};
