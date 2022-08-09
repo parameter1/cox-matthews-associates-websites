a90eb2c99cc948707d39e1b0a184113f4a6a50f7
Jessica Wade <jessica.wade@parameter1.com>
2022-08-09 13:35:47 -0500

    Move ‘Top Women’ under ‘Top 100’ in nav

--- sites/diverseeducation.com/config/navigation.js
+++ sites/diverseeducation.com/config/navigation.js
@@ -34,10 +34,10 @@ const awards = [
   { href: '/awards-honors/dr-john-hope-franklin', label: 'Dr. John Hope Franklin Award' },
   { href: '/awards-honors/emerging-scholars/2022', label: 'Emerging Scholars' },
   { href: '/awards-honors/graduate-scholars/2022', label: 'Graduate Scholars' },
-  { href: '/awards-honors/top-women', label: 'Top Women' },
   { href: '/awards-honors/mppwcc', label: 'Most Promising Places to Work: Community Colleges' },
   { href: '/awards-honors/mppwsa', label: 'Most Promising Places to Work: Student Affairs' },
   { href: 'https://top100.diverseeducation.com', label: 'Top 100', target: '_blank' },
+  { href: '/awards-honors/top-women', label: 'Top Women' },
 ];
 
 const utilities = [
