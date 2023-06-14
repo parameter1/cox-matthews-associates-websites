a530f84d13213fb3e263210cc583dc62062011e8
Brian Miller <brian@parameter1.com>
2023-06-14 14:32:57 -0500

    Update routes and templates with new content meter
     - Update DE routing to content meter & newsletter middelwares
     - Add content meter to document
     - DE site level content meter config

--- sites/diverseeducation.com/server/routes/diverse-poll.js
+++ sites/diverseeducation.com/server/routes/diverse-poll.js
@@ -1,7 +1,9 @@
+const { newsletterState } = require('@cox-matthews-associates/package-global/middleware/newsletter-state');
+
 const diversePoll = require('../templates/static-pages/diverse-poll');
 
 module.exports = (app) => {
-  app.get('/diverse-poll', (req, res) => {
+  app.get('/diverse-poll', newsletterState(), (req, res) => {
     res.marko(diversePoll);
   });
 };
--- sites/diverseeducation.com/server/routes/subscribe.js
+++ sites/diverseeducation.com/server/routes/subscribe.js
@@ -1,7 +1,9 @@
+const { newsletterState } = require('@cox-matthews-associates/package-global/middleware/newsletter-state');
+
 const subscribe = require('../templates/subscribe');
 
 module.exports = (app) => {
-  app.get('/subscribe', (_, res) => {
+  app.get('/subscribe', newsletterState(), (_, res) => {
     res.marko(subscribe);
   });
 };
