67b390b69d128e8fcd68e2ccad0dcc7519349ce1
Flavio Galizia <flaviogalizia@Flavios-MacBook-Pro.local>
2023-07-05 11:40:40 -0400

    update graduate scholars url on hamburger menu

--- sites/diverseeducation.com/config/navigation.js
+++ sites/diverseeducation.com/config/navigation.js
@@ -33,7 +33,7 @@ const awards = [
   { href: 'https://www.diverseeducation.com/page/DOIT', label: 'DOIT', target: '_blank' },
   { href: '/awards-honors/dr-john-hope-franklin', label: 'Dr. John Hope Franklin Award' },
   { href: '/awards-honors/emerging-scholars/2023', label: 'Emerging Scholars' },
-  { href: '/awards-honors/graduate-scholars/2022', label: 'Graduate Scholars' },
+  { href: '/awards-honors/graduate-scholars/2023', label: 'Graduate Scholars' },
   { href: '/awards-honors/mppwcc', label: 'Most Promising Places to Work: Community Colleges' },
   { href: '/awards-honors/mppwsa', label: 'Most Promising Places to Work: Student Affairs' },
   { href: 'https://top100.diverseeducation.com', label: 'Top 100', target: '_blank' },
