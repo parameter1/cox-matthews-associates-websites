e668fecd2489a1269c2fd0c6093cb1ea2dff10ff
Jessica Wade <jessica.wade@parameter1.com>
2023-08-15 13:34:07 -0500

    Add ‘promotion’ ad slot

--- sites/diverseeducation.com/config/native-x.js
+++ sites/diverseeducation.com/config/native-x.js
@@ -7,6 +7,7 @@ config.enabled = true;
 config
   .setAliasPlacements('default', [
     { name: 'default', id: '61f837a0a182000001d6e5f4' },
+    { name: 'promotion', id: '64db7690ae9a5e0001fc90f1' },
   ]);
 
 module.exports = config;
