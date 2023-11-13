12107bdaf43f05a7442fe4ec8a6c521ac1ca94af
Shinsina <shinsina@att.net>
2023-11-13 12:21:53 -0600

    Replace logo URL with new version for staging

--- sites/diverseeducation.com/config/site.js
+++ sites/diverseeducation.com/config/site.js
@@ -37,9 +37,9 @@ module.exports = {
   },
   logos: {
     navbar: {
-      src: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/DE-40year-anniversary.png?h=60&auto=format,compress&q=70',
+      src: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/Diverse-Logo-2023-BlueWithTag.png?h=60&auto=format,compress&q=70',
       srcset: [
-        'https://img.diverseeducation.com/files/base/diverse/all/image/static/DE-40year-anniversary.png?h=60&auto=format,compress&q=70&dpr=2 2x',
+        'https://img.diverseeducation.com/files/base/diverse/all/image/static/Diverse-Logo-2023-BlueWithTag.png?h=60&auto=format,compress&q=70&dpr=2 2x',
       ],
     },
     footer: {
