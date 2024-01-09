932fc4404846fc59fe075a5b4fbdef899763cec4
Jessica Wade <jessica.wade@parameter1.com>
2024-01-09 13:14:23 -0600

    Change logo to 40th Anniversary

--- sites/diverseeducation.com/config/site.js
+++ sites/diverseeducation.com/config/site.js
@@ -38,15 +38,15 @@ module.exports = {
   },
   logos: {
     navbar: {
-      src: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/Diverse-Logo-2023-BlueWithTag.png?h=60&auto=format,compress&q=70',
+      src: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/DIV-40th-Anniversary.png?h=60&auto=format,compress&q=70',
       srcset: [
-        'https://img.diverseeducation.com/files/base/diverse/all/image/static/Diverse-Logo-2023-BlueWithTag.png?h=60&auto=format,compress&q=70&dpr=2 2x',
+        'https://img.diverseeducation.com/files/base/diverse/all/image/static/DIV-40th-Anniversary.png?h=60&auto=format,compress&q=70&dpr=2 2x',
       ],
     },
     footer: {
-      src: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/Diverse-Logo-2023-WhiteWithTag-01.png?h=45&auto=format,compress&q=70',
+      src: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/DIV-40th-Anniversary.png?h=45&auto=format,compress&q=70',
       srcset: [
-        'https://img.diverseeducation.com/files/base/diverse/all/image/static/Diverse-Logo-2023-WhiteWithTag-01.png?h=45&auto=format,compress&q=70&dpr=2 2x',
+        'https://img.diverseeducation.com/files/base/diverse/all/image/static/DIV-40th-Anniversary.png?h=45&auto=format,compress&q=70&dpr=2 2x',
       ],
     },
   },
