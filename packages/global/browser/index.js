d805943b1339b2dd773928a8bf601c562d0bff9f
Jessica Wade <jessica.wade@parameter1.com>
2024-11-07 10:37:27 -0600

    Create track-content-meter vue compontent and add to index.js

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -3,10 +3,12 @@ import ContactUs from '@parameter1/base-cms-marko-web-contact-us/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
 const GlobalPremiumEmployers = () => import(/* webpackChunkName: "global-premium-employers" */ './premium-employers.vue');
+const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
 
 export default (Browser) => {
   GCSE(Browser);
   ContactUs(Browser);
   MonoRail(Browser);
   Browser.register('GlobalPremiumEmployers', GlobalPremiumEmployers);
+  Browser.register('ContentMeterTrack', ContentMeterTrack);
 };
