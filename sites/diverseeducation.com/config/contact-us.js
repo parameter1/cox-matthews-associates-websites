d5b1ea4a2814a1f41d9c398b77b3142d97372781
Jessica Wade <jessica.wade@parameter1.com>
2025-09-18 14:41:24 -0500

    Update contact us image

--- sites/diverseeducation.com/config/contact-us.js
+++ sites/diverseeducation.com/config/contact-us.js
@@ -1,6 +1,6 @@
 module.exports = {
   branding: {
-    logo: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/diverse-education-logo.png?h=60&auto=format,compress&q=70',
+    logo: 'https://img.diverseeducation.com/files/base/diverse/all/image/static/EDULEDGER.png?h=60&auto=format,compress&q=70',
   },
   submitPodcast: {
     title: 'Submit A podcast',
