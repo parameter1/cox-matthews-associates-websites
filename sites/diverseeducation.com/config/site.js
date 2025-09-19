ee887fc9f76f3ed1b852c518c06475513f499e69
Jessica Wade <jessica.wade@parameter1.com>
2025-09-19 09:00:50 -0500

    Add annoucement text

--- sites/diverseeducation.com/config/site.js
+++ sites/diverseeducation.com/config/site.js
@@ -15,6 +15,10 @@ const contactUs = require('./contact-us');
 const contentMeter = require('./content-meter');
 
 module.exports = {
+  announcement: {
+    title: 'We\'ve Evolved! Introducing the new The EDULedger.com!',
+    shortTitle: 'Introducing the new The EDULedger.com!',
+  },
   suggestedVideoID: process.env.SUGGESTED_VIDEO_ID,
   suggestedVideoPlaylist: process.env.SUGGESTED_VIDEO_PLAYLIST,
   useLinkInjectedBody: process.env.USE_LINK_INJECTED_BODY === 'true',
