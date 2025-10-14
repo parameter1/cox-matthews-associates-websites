7a22d166a5b9a288b98e02263cb5c8faf07b79e5
Shinsina <shinsina@att.net>
2025-10-14 10:38:18 -0500

    Setup CCNewsNow search

--- sites/ccnewsnow.com/config/search.js
+++ sites/ccnewsnow.com/config/search.js
@@ -1,1 +1,18 @@
-module.exports = {};
+module.exports = {
+  contentTypes: ['Article'],
+  assignedToWebsiteSectionIds: [
+    75580,
+    75581,
+    75582,
+    75583,
+    75584,
+    75585,
+    75586,
+    75587,
+    75588,
+    75589,
+  ],
+  defaultSortField: 'SCORE',
+  type: 'MATCH_PHRASE',
+  useMindful: true,
+};
