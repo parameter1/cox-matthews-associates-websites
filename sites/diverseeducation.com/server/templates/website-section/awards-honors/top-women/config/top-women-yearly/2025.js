abf40ad8adb33f69fc1b9bec7098fa7177a83211
Flavio Galizia <flaviogalizia@Flavios-MBP.lan>
2026-02-10 11:02:52 -0500

    replace diverse with the edu ledger on women in higher ed landing pages

--- sites/diverseeducation.com/server/templates/website-section/awards-honors/top-women/config/top-women-yearly/2025.js
+++ sites/diverseeducation.com/server/templates/website-section/awards-honors/top-women/config/top-women-yearly/2025.js
@@ -1,4 +1,4 @@
 module.exports = {
   title: 'Celebrating Leading Women In Higher Education',
-  subtitle: '<i>Diverse</i> presents 40 women making a difference in the world of academe.',
+  subtitle: '<i>The Edu Ledger</i> presents 40 women making a difference in the world of academe.',
 };
