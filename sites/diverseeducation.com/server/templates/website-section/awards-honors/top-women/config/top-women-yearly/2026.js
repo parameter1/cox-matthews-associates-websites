57b26cd1eb436c9cb0e1cb9b6f881ceacfbdab02
Flavio Galizia <flaviogalizia@Flavios-MBP.lan>
2026-03-06 16:40:00 -0500

    correction to Women in Higher Ed landing page

--- sites/diverseeducation.com/server/templates/website-section/awards-honors/top-women/config/top-women-yearly/2026.js
+++ sites/diverseeducation.com/server/templates/website-section/awards-honors/top-women/config/top-women-yearly/2026.js
@@ -1,4 +1,4 @@
 module.exports = {
   title: 'Celebrating Leading Women In Higher Education',
-  subtitle: '<i>The Edu Ledger</i> presents 25 women making a difference in the world of academe.',
+  subtitle: '<i>The Edu Ledger</i> presents 26 women making a difference in the world of academe.',
 };
